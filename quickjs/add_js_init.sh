000653133148c9955947d9b258323f8c5b14288c
Bob <bob.lund@gmail.com>
2025-07-08 06:40:56 -0600

    qjsc-style mod init

--- quickjs/add_js_init.sh
+++ quickjs/add_js_init.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# $1 = js_init_function name
+#!/bin/bash
+
+# $1 = module name
 # $2 = c file with main() generated from js
 
 SED_CMD=$( ( if [[ $(sed --version) =~ 'GNU sed' ]]; then echo "sed -i"; else echo "sed -i ''"; fi ) 2>/dev/null)
-${SED_CMD} "/#include \"quickjs-libc.h\"/a\\
-void $1(JSContext *);
-/ctx = JS_NewCustomContext(rt);/a\\
-$1( ctx );
+${SED_CMD} "/return ctx;/i\\
+	{ \\
+    extern JSModuleDef *js_init_module_${1}(JSContext *ctx, const char *name); \\
+    js_init_module_${1}(ctx, \"${1}\"); \\
+  }
 " $2
