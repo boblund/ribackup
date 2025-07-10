fb43be4715086f928f1d4f250b2c683dccbf55cb
Bob <bob.lund@gmail.com>
2025-07-10 17:38:42 -0600

    perl script

--- quickjs/qjs_env.sh
+++ quickjs/qjs_env.sh
@@ -1,14 +1,15 @@
 # returns location of quickjs lib and include directories
-
-path=$(which qjsc 2>/dev/null)
-case "$path" in
-  *homebrew*)
-    echo $(brew list quickjs|grep -m 1 include|sed -e 's/\(^.*\)include.*/\1/')
-    ;;
-	/usr/local*)
-		echo /usr/local
-		;;
-  *)
-    echo ""
-    ;;
-esac
+echo `which qjsc` | perl -F'/' -ane 'print "/" . join("/", $F[1], $F[2])'
+#echo `which qjsc` | sed -E 's|^(/[^/]+/[^/]+).*|\1|'
+#path=$(which qjsc 2>/dev/null)
+#case "$path" in
+#  *homebrew*)
+#    echo $(brew list quickjs|grep -m 1 include|sed -e 's/\(^.*\)include.*/\1/')
+#    ;;
+#	/usr/local*)
+#		echo /usr/local
+#		;;
+#  *)
+#    echo ""
+#    ;;
+#esac
